c003cc5d980fd8eb00675ec588574c1a7bda7155
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-05 01:29:00 +0700

    Intial project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Routing
+// @ts-ignore
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// Components
+import Home from './components/Home';
+import Notfound from './components/Notfound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+    <Router>
+        <Switch>
+            <Route path='/'>
+                <Home />
+            </Route>
+            <Route path='/*'>
+                <Notfound />
+            </Route>
+        </Switch>
+    </Router>
+);
 
 export default App;
