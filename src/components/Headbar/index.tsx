3322c85a2e7857ca09b53f64815e9ef3199f50d5
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-05 20:20:41 +0700

    Change file name Headbar.ts > index.ts

--- src/components/Headbar/index.tsx
+++ src/components/Headbar/index.tsx
@@ -5,11 +5,14 @@ import threeDotImg from '../../images/ThreeDot.png';
 
 import { Wrapper, Content, BackIcon, ThreeDotIcon } from "./Headbar.styles";
 
-const Headbar: React.FC = () => (
+type Prop = {
+    header: string
+}
+const Headbar: React.FC<Prop> = ({ header }) => (
     <Wrapper>
         <Content>
             <BackIcon src={backIconImg} alt="back icon" />
-            <h2>dreamteam101</h2>
+            <h2>header</h2>
             <ThreeDotIcon src={threeDotImg} alt="three dot icon" />
         </Content>
     </Wrapper>
