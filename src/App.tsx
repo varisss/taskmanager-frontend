8c042f12e8668e0830da28d0a97166cc10823125
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-05 17:00:41 +0700

    Globalstyling

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 // Routing
 // @ts-ignore
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Components
 import Home from './components/Home';
 import Notfound from './components/Notfound';
+import Headbar from './components/Headbar';
+import Project from './components/Project/Project';
 
-const App: React.FC = () => (
-    <Router>
-        <Switch>
-            <Route path='/'>
-                <Home />
-            </Route>
-            <Route path='/*'>
-                <Notfound />
-            </Route>
-        </Switch>
-    </Router>
-);
+const App: React.FC = () => {
+    const [projectId, setProjectId] = useState('');
+
+    return (
+        <Router>
+            <Headbar />
+            <Switch>
+                <Route path='/'>
+                    <Home />
+                </Route>
+                <Route path={`/${projectId}`}>
+                    <Home />
+                </Route>
+                <Route path='/*'>
+                    <Notfound />
+                </Route>
+            </Switch>
+        </Router>
+    )
+};
 
 export default App;
