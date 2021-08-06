0ecc6ec731a73d8892639ed008de53928a0c2cdd
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-08-07 03:25:16 +0700

    add dropshow to AddButton

--- src/components/Buttons/AddButton/index.tsx
+++ src/components/Buttons/AddButton/index.tsx
@@ -7,14 +7,21 @@ import { IoIosAddCircle } from "react-icons/io";
 import { Wrapper } from "./AddButton.styles";
 
 interface Prop {
-    callback: () => void
+  callback: () => void;
 }
 
 const AddButton: React.FC<Prop> = ({ callback }) => (
-    <Wrapper>
-        <IoIosAddCircle onClick={callback} color='#803300' size='100%' />
-    </Wrapper>
-
-)
+  <Wrapper>
+    <IoIosAddCircle
+      onClick={callback}
+      color="#803300"
+      size="100%"
+      style={{
+        boxShadow:
+          "box-shadow: 0px 4px 5px rgba(128, 51, 0, 0.14), 0px 1px 10px rgba(128, 51, 0, 0.12), 0px 2px 4px rgba(128, 51, 0, 0.2)",
+      }}
+    />
+  </Wrapper>
+);
 
 export default AddButton;
